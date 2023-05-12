bc84c8f95c308305ec7e105154521abeb089e5c9
Jimbo <majidshahbani@gmail.com>
2023-05-13 00:05:39 +0330

    feat(background): initialize background script

--- core/background.js
+++ core/background.js
@@ -1,9 +1,18 @@
 import { ReviewsRepository } from './reviews/reviews.repository.js'
-import { ReviewsService } from './reviews/reviews.service'
+import { ReviewsService } from './reviews/reviews.service.js'
+import { config } from './config.js'
+import { registerScripts } from './scripts.js'
+import { BrowserContext } from './browser/browser.context.js'
 
 async function bootstrap () {
-  const reviewsRepository = ReviewsRepository({ config })
+  const browserContext = BrowserContext({ context: chrome??browser })
+  browserContext.onAddonInstalled(()=>{
+    console.log('hehehehehehereree');
+  })
+  const reviewsRepository = ReviewsRepository({ config, browserContext })
+  await reviewsRepository.initialize()
   const reviewsService = ReviewsService({ reviewsRepository })
+  await registerScripts({browserContext})
 }
 
 bootstrap()
