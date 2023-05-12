c38246fa99b9f92010f02ce75fd1b90259655179
Jimbo <majidshahbani@gmail.com>
2023-05-13 00:05:13 +0330

    feat(reviews): initialize repository

--- core/reviews/reviews.repository.js
+++ core/reviews/reviews.repository.js
@@ -1,1 +1,17 @@
-export const ReviewsRepository = () => {}
+export const ReviewsRepository = ({ config, browserContext }) => {
+  return {
+    async initialize () {
+      const cachedCompanies = await browserContext.getFromStorage('companies')
+      if (cachedCompanies?.length) return
+      const { companies, error } = await fetch(config.REVIEWS_RESOURCE_URL)
+        .then(async response => {
+          const companies = await response.json()
+          return { companies }
+        })
+        .catch(error => ({ error }))
+      if (error) console.error({ error })
+      await browserContext.setToStorage('companies', JSON.stringify(companies))
+    },
+    async findOneByDomain () {}
+  }
+}
